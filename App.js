af7154ba49986c7f49da390a27bdd02ba23a2225
Faris-Ihsan <farisihsan32@yahoo.co.id>
2025-08-20 14:10:32 +0700

    87. Größenverhältnisse mit unterschiedlichen flex-Werten festlegen

--- App.js
+++ App.js
@@ -13,17 +13,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // flexdirection: 'column'
   },
   box1: {
-    flex: 1,
+    flex: 1, // Größeverhältnisse (zwischen "Geschwistern")
     backgroundColor: 'cyan',
   },
   box2: {
-    flex: 1,
+    flex: 2,
     backgroundColor: 'magenta',
   },
   box3: {
-    flex: 1,
+    flex: 3,
     backgroundColor: 'yellow',
   },  
 });
