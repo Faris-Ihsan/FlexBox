e4ef4971d2307646ecb9a02ce47827ee25446ddb
Faris-Ihsan <farisihsan32@yahoo.co.id>
2025-08-20 14:34:51 +0700

    90. alignItems legt die Anordnung der FlexBox auf der Querachse fest

--- App.js
+++ App.js
@@ -13,8 +13,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row', // Ausrichtung der Hauptsache 
+    flexDirection: 'column', // Ausrichtung der Hauptsache 
     justifyContent: 'space-evenly', // Anordnung der Elemente auf der Hauptachse
+    alignItems: 'center', // Anordnung auf Querachse
   },
   box:{
     width: 100,
@@ -33,3 +34,7 @@ const styles = StyleSheet.create({
 // justifyContent legt Anordnung der Elements auf der Hauptachse fest
 // justifyContent: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly'
 // justifyContent hat 'flex-start' als Default
+
+// alignItems: Anordnung auf der Querachse (senkrecht zur Hauptachsel)
+// alignItems: 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'baseline'
+// alignItems hat 'stretch' als Default
